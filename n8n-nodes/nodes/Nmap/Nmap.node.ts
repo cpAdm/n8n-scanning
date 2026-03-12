85605baf6f3340e7b0c1b2f84dc5a572680c22ac
cpadm <57954026+cpAdm@users.noreply.github.com>
2026-03-12 15:52:20 +0100

    chore: Update formatting commands and refactor imports for consistency

--- n8n-nodes/nodes/Nmap/Nmap.node.ts
+++ n8n-nodes/nodes/Nmap/Nmap.node.ts
@@ -1,6 +1,12 @@
 import { IExecuteFunctions, INodeType, INodeTypeDescription } from 'n8n-workflow';
 import { writeDataFile } from '../../utils/file';
-import { createNotice, executeTool, getParams, ScannerDescription, ScannerProperties } from '../ScannerBase';
+import {
+	createNotice,
+	executeTool,
+	getParams,
+	ScannerDescription,
+	ScannerProperties,
+} from '../ScannerBase';
 
 // noinspection JSUnusedGlobalSymbols, refered in package.json
 export class Nmap implements INodeType {
